1a94ffb8f81414793551b32e85a9f776e50c406c
SergeyDedikov <serded59@yandex.ru>
2022-03-04 17:00:15 +0500

    feat: change cemantic

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,18 +7,18 @@ function AboutProject() {
       <SectionTitle id={"about-project"} title={"О проекте"} />
       <ul className="description">
         <li className="description__container">
-          <h3 className="description__title">
+          <p className="description__title">
             Дипломный проект включал 5&nbsp;этапов
-          </h3>
+          </p>
           <p className="description__paragraph">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и&nbsp;финальные доработки.
           </p>
         </li>
         <li className="description__container">
-          <h3 className="description__title">
+          <p className="description__title">
             На&nbsp;выполнение диплома ушло 5&nbsp;недель
-          </h3>
+          </p>
           <p className="description__paragraph">
             У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые
             нужно было соблюдать, чтобы успешно защититься.
