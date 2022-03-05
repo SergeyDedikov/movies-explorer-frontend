69092677813058f5d4012f54f5b19167cfc30ade
SergeyDedikov <serded59@yandex.ru>
2022-03-05 19:35:17 +0500

    feat: component Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,25 @@
 import "./Header.css";
 
 function Header() {
-  return(
-    <div className="container">Header</div>
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <nav className="header__nav-movies"></nav>
+      <nav className="header__nav-authentication">
+        <ul className="header__nav-links">
+          <li>
+            <a className="header__link header__link_register" href="#register">
+              Регистрация
+            </a>
+          </li>
+          <li>
+            <a className="header__link header__link_login" href="#login">
+              Войти
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
