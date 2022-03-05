e4c1f32f621efc1e3900556b26992fe1d8283041
SergeyDedikov <serded59@yandex.ru>
2022-03-05 18:02:31 +0500

    feat: component Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,52 @@
 import "./Footer.css";
 
 function Footer() {
-  return(
-    <div className="container">Footer</div>
+  return (
+    <footer className="footer">
+      <p className="footer__text footer__text_place_description">
+        Учебный проект Яндекс.Практикум х&nbsp;BeatFilm.
+      </p>
+      <div className="footer__underline">
+        <p className="footer__text footer__text_place_copyright">
+          &copy;&nbsp;2022
+        </p>
+
+        <nav>
+          <ul className="footer__social-links">
+            <li>
+              <a
+                className="footer__text footer__text_place_social-link"
+                href="https://practicum.yandex.ru"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer__text footer__text_place_social-link"
+                href="https://github.com/SergeyDedikov"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer__text footer__text_place_social-link"
+                href="https://facebook.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
   );
 }
 
