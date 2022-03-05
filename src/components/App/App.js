cb4297e8926bbe8b3ff243b1365b54a7fbb5e77b
SergeyDedikov <serded59@yandex.ru>
2022-03-05 16:48:27 +0500

    feat: component Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
-import Promo from "../Promo/Promo.js";
-import NavTab from "../NavTab/NavTab";
-import AboutProject from "../AboutProject/AboutProject.js";
-import Techs from "../Techs/Techs.js";
-import AboutMe from "../AboutMe/AboutMe.js";
+import Main from "../Main/Main";
 
 function App() {
   return (
-    <main>
-      <Promo />
-      <NavTab />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-    </main>
+    <>
+      <Main />
+    </>
   );
 }
 
