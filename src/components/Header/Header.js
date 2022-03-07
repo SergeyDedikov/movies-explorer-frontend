7e46f25a8fe50cc2a27e953d985c4f6d55fae7db
SergeyDedikov <serded59@yandex.ru>
2022-03-07 16:08:39 +0500

    feat: component Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,11 @@
 import "./Header.css";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__logo"></div>
-      <nav className="header__nav-movies"></nav>
-      <nav className="header__nav-authentication">
-        <ul className="header__nav-links">
-          <li>
-            <a className="header__link header__link_register" href="#register">
-              Регистрация
-            </a>
-          </li>
-          <li>
-            <a className="header__link header__link_login" href="#login">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,30 @@
 import "./Navigation.css";
 
 function Navigation() {
-  return(
-    <div className="container">Navigation</div>
+  return (
+    <div className="navigation">
+      <nav className="navigation__movies"></nav>
+      <nav className="navigation__authentication">
+        <ul className="navigation__links">
+          <li>
+            <a
+              className="navigation__link navigation__link_register"
+              href="#register"
+            >
+              Регистрация
+            </a>
+          </li>
+          <li>
+            <a
+              className="navigation__link navigation__link_login"
+              href="#login"
+            >
+              Войти
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
