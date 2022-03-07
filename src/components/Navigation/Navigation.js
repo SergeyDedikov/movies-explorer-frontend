9514927862fb3b69720c9f60e8f2e910cb5fdafc
SergeyDedikov <serded59@yandex.ru>
2022-03-07 16:42:02 +0500

    feat: refactor Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,9 +3,28 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="navigation">
-      <nav className="navigation__movies"></nav>
+      <nav className="navigation__movies">
+        <ul className="navigation__links navigation__links_movies">
+          <li>
+            <a
+              className="navigation__link navigation__link_movies"
+              href="#movies"
+            >
+              Фильмы
+            </a>
+          </li>
+          <li>
+            <a
+              className="navigation__link navigation__link_saved-movies"
+              href="#saved-movies"
+            >
+              Сохранённые фильмы
+            </a>
+          </li>
+        </ul>
+      </nav>
       <nav className="navigation__authentication">
-        <ul className="navigation__links">
+        <ul className="navigation__links navigation__links_authentication">
           <li>
             <a
               className="navigation__link navigation__link_register"
