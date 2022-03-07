654698484da2bf9230b98c9f31aacf7ff38cdcd4
SergeyDedikov <serded59@yandex.ru>
2022-03-07 17:38:35 +0500

    feat: plug Movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import Movies from "../Movies/Movies";
 
 function App() {
   return (
     <>
       <Header />
+      <Movies />
       {/* <Main /> */}
       <Footer />
     </>
