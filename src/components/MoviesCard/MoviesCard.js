e58a4317e364596eb474f62d81c7b2da37b4de02
SergeyDedikov <serded59@yandex.ru>
2022-03-09 17:17:11 +0500

    feat: components MoviesCardList and MoviesCard

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,12 +1,51 @@
 import "./MoviesCard.css";
 
-function MoviesCard({ title, image, duration }) {
+function MoviesCard({ movie }) {
+  // преобразуем длительность фильма в часы с минутами
+  function formatMinutes(value) {
+    let minuteTime = value; // минуты
+    let hourTime = 0; // часы
+
+    if (minuteTime > 60) {
+      hourTime = parseInt(minuteTime / 60); // получим часы
+      minuteTime = parseInt(minuteTime % 60); // остаток минут
+    }
+
+    let result;
+
+    if (minuteTime > 0) {
+      result = minuteTime + "м";
+    }
+    if (hourTime > 0) {
+      result = hourTime + "ч " + result;
+    }
+    return result;
+  }
+
+  // временное условие для определения лайка
+  const isLiked = movie.owner === 111;
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const movieLikeButtonClassName = `movies-card__button-like ${
+    isLiked && "movies-card__button-like_active"
+  }`;
+
   return (
-    <li className="movies-card">
-      <h4 className="movies-card__title">{title}</h4>
-      <p className="movies-card__duration">{duration}</p>
-      <div className="movies-card__like"></div>
-      <img className="movies__image" src={image} alt={title} />
+    <li>
+      <figure className="movies-card">
+        <figcaption className="movies-card_info">
+          <h2 className="movies-card__title">{movie.nameRU}</h2>
+          <p className="movies-card__duration">
+            {formatMinutes(movie.duration)}
+          </p>
+          <button className={`${movieLikeButtonClassName} button`}></button>
+        </figcaption>
+        <img
+          className="movies-card__image"
+          src={movie.image}
+          alt={movie.nameRU}
+        />
+      </figure>
     </li>
   );
 }
