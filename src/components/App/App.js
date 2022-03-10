e3819fe99bd8e628209258f5140d78da6464abdd
SergeyDedikov <serded59@yandex.ru>
2022-03-10 20:28:19 +0500

    feat: component 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import NotFound from "../NotFound/NotFound";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <section className="not-found" aria-label="Ничего не найдено">
+      <h2 className="not-found__title">404</h2>
+    </section>
+  );
+}
