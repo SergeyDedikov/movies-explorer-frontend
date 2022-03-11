e8c58015d7207407d99c18e38d22ec1f9413576e
SergeyDedikov <serded59@yandex.ru>
2022-03-11 15:44:29 +0500

    feat: shange visible Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,24 @@ import Navigation from "../Navigation/Navigation";
 
 function Header() {
   const { pathname } = useLocation();
-  const isBasePath = pathname === "/";
 
-  return (
-    <header className={`header ${!isBasePath && "header_background_none"}`}>
-      <Link to="/" className="header__logo"></Link>
-      <Navigation />
-    </header>
-  );
+  const isBasePath = pathname === "/";
+  const isHeaderPath =
+    pathname === "/" ||
+    pathname === "/movies" ||
+    pathname === "/saved-movies" ||
+    pathname === "/profile";
+
+  if (isHeaderPath) {
+    return (
+      <header className={`header ${!isBasePath && "header_background_none"}`}>
+        <Link to="/" className="header__logo"></Link>
+        <Navigation />
+      </header>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Header;
