41afca85ec03438560f4705f31e49fd8f859ec62
SergeyDedikov <serded59@yandex.ru>
2022-03-11 19:36:54 +0500

    feat: adaptive 768 refactor AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,46 +6,46 @@ import Portfolio from "../Portfolio/Portfolio.js";
 function AboutMe() {
   return (
     <Section name={"student"} title={"Студент"}>
-      <div className="student__info">
+      <div className="student__info-container">
+        <div className="student__description">
+          <h3 className="student__title">Сергей</h3>
+          <p className="student__subtitle">Веб-разработчик, 43 года</p>
+          <p className="student__paragraph">
+            Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет
+            экономики СГУ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю
+            слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить.
+            С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;.
+            После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься
+            фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.
+          </p>
+          <ul className="student__social-links">
+            <li>
+              <a
+                className="student__social-link"
+                href="https://facebook.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a
+                className="student__social-link"
+                href="https://github.com/SergeyDedikov"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
         <img
           src={photo}
           className="student__photo"
           alt="Фотография студента"
         ></img>
-        <h3 className="student__title">Сергей</h3>
-        <p className="student__subtitle">Веб-разработчик, 43 года</p>
-        <p className="student__paragraph">
-          Я&nbsp;родился и&nbsp;живу в&nbsp;городе Чайковском Пермского края,
-          учился по&nbsp;специальности &laquo;Техническое обслуживание
-          вычилительной техники&raquo;. Я&nbsp;люблю слушать инструментальную
-          музыку. Занимаюсь кроссом на&nbsp;велосипеде, зимой &mdash;
-          на&nbsp;лыжах. Есть собака и&nbsp;кошка. В&nbsp;2021 году поступил
-          на&nbsp;курсы &laquo;Веб-разработчик&raquo; Я.Практикума. Хочу стать
-          кодером в&nbsp;хорошей компании.
-        </p>
-
-        <ul className="student__social-links">
-          <li>
-            <a
-              className="student__social-link"
-              href="https://facebook.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Facebook
-            </a>
-          </li>
-          <li>
-            <a
-              className="student__social-link"
-              href="https://github.com/SergeyDedikov"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </li>
-        </ul>
       </div>
       <Portfolio />
     </Section>
