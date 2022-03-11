21b597bb74c7e108a91209cc1c79e9cc9f892c1d
SergeyDedikov <serded59@yandex.ru>
2022-03-11 15:21:38 +0500

    feat: create component 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 import "./NotFound.css";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  function gotoBack() {
+    navigate(-1);
+  }
+
   return (
-    <section className="not-found" aria-label="Ничего не найдено">
-      <h2 className="not-found__title">404</h2>
+    <section className="not-found" aria-label="Страница не найдена">
+      <p className="not-found__title">404</p>
+      <p className="not-found__text">Страница не найдена</p>
+      <button onClick={gotoBack} className="not-found__button button">
+        Назад
+      </button>
     </section>
   );
 }
