4466e4e58aaee1630472b0bffd3be5a46bc34c51
SergeyDedikov <serded59@yandex.ru>
2022-03-12 15:02:55 +0500

    feat: adaptive 768 MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
+
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import AddMoreMovies from "../AddMoreMovies/AddMoreMovies";
 
 function MoviesCardList({ movies, isSavedMovies }) {
+  const [moviesList, setMoviesList] = useState(movies);
+
   // условие для отображения кнопки Ещё (временное)
   const isMoreMovies = movies.length > 11;
+
+  // изменим число отображаемых карточек при разной ширине экрана
+  useEffect(() => {
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+      setMoviesList(movies.slice(0, 12));
+    } else if (window.matchMedia("(min-width: 500px)").matches) {
+      setMoviesList(movies.slice(0, 8));
+    }
+  });
 
   return (
     <section className="movies-card-list" aria-label="Список фильмов">
       <ul className="movies__list">
-        {movies.map((movieItem) => (
+        {moviesList.map((movieItem) => (
           <MoviesCard
             key={movieItem.movieId}
             movie={movieItem}
