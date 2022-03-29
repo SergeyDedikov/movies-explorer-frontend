1eb616a479c2f2c0acbd69fdaa6f2d64411a1a2b
SergeyDedikov <serded59@yandex.ru>
2022-03-29 21:08:57 +0500

    feat: delete not important

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,6 @@
 import { BASE_URL_MAIN_API } from "./constants";
 
 const BASE_URL = "http://localhost:3001";
-// const BASE_URL = "https://api.movies-favorite.nomoredomains.work";
 
 class Auth {
   constructor(url) {
