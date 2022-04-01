db3494743dfcdf6db35ca29544edfceaa7d1dd66
SergeyDedikov <serded59@yandex.ru>
2022-04-01 19:22:49 +0500

    bugfix: add validator for Email

--- src/hooks/form-validation.js
+++ src/hooks/form-validation.js
@@ -1,4 +1,5 @@
 import React, { useCallback } from "react";
+import { isEmail } from "validator";
 
 //хук управления формой
 export function useForm() {
@@ -24,8 +25,19 @@ export function useFormWithValidation() {
     const target = event.target;
     const name = target.name;
     const value = target.value;
+    const type = target.type;
+
     setValues({ ...values, [name]: value });
-    setErrors({ ...errors, [name]: target.validationMessage });
+
+    if (type === "email" && !isEmail(value)) {
+      setErrors({
+        ...errors,
+        [name]: "Введите корректный формат E-mail по типу email@example.com",
+      });
+    } else {
+      setErrors({ ...errors, [name]: target.validationMessage });
+    }
+
     setIsValid(target.closest("form").checkValidity());
   };
 
